f3cdf72b0941aaf5432164f323850e5ed49b4229
Pufferfish-Kirby <jamesroma14@gmail.com>
2025-04-08 14:58:07 -0400

    modified App.js - JR

--- multi-fx-app/src/App.js
+++ multi-fx-app/src/App.js
@@ -6,7 +6,14 @@ export default function App() {
   
     return (
       <>
-        <h1>Multi Fx</h1>
+      <div style={{display: "flex", alignItems:"center", gap: "1rem"}}>
+        <img
+          src="https://raw.githubusercontent.com/dangerousfroggo/YRHACKSREPO/refs/heads/main/2025_04_08_0o6_Kleki.png?token=GHSAT0AAAAAADB36FMXJLQQMSJ63AHDBBDKZ7VOACA"
+          alt="Logo"
+          style={{width: "50px", height:"50px" }}
+        img/>
+        <h1>style={{margin: "0"}}Multi Fx</h1>
+        </div>
         <EffectBoxes />
       </>
     );
