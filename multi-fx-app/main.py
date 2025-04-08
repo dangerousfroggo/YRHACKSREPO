b8468229d7cfbb6ede2a3498f026707ad7e2d0f5
Sergei <serg.borischev2017@gmail.com>
2025-04-08 14:09:59 -0400

    updated distortion

--- multi-fx-app/main.py
+++ multi-fx-app/main.py
@@ -1,8 +1,7 @@
-import sys
 import pyaudio
 import numpy as np
 import psutil, os
-# psutil.Process(os.getpid()).nice(psutil.HIGH_PRIORITY_CLASS)
+psutil.Process(os.getpid()).nice(psutil.HIGH_PRIORITY_CLASS)
 
 CHUNK = 3
 RATE = 96000
@@ -13,30 +12,11 @@ p = pyaudio.PyAudio()
 stream = p.open(format=pyaudio.paInt16, channels=1, rate=RATE, input=True, frames_per_buffer=CHUNK)
 player = p.open(format=pyaudio.paInt16, channels=1, rate=RATE, output=True, frames_per_buffer=CHUNK)
 
-currentEffect = None
-
-# Default values
-VOL = int(sys.argv[1]) if len(sys.argv) > 1 else 1
-GAIN = int(sys.argv[2]) if len(sys.argv) > 2 else 1
-WET_OR_DRY = int(sys.argv[3]) if len(sys.argv) > 3 else 1
-
-print(f"Starting distortion with VOL={VOL}, GAIN={GAIN}, WET_OR_DRY={WET_OR_DRY}")
 
 
 
 class Distortion:
     def __init__(self, type, volume, gain, enabled):
-        self.type = type
-        self.volume = volume
-        self.gain = gain
-        self.enabled = enabled
-    def process():
-        pass
-
-distortion1 = Distortion("distortion", VOL, GAIN, WET_OR_DRY)
-
-        
-    
         self.type = type            # 3
         self.volume = volume        # 0.0 to 1.0
         self.gain = gain            # 0.0 to 1.0
